39ce5a4fd8019490785c72482b6e0c51eb32cdfc
helmiapriyanda <56975427+helmiapriyanda@users.noreply.github.com>
2020-11-24 13:29:49 +0700

    latihan

---
+++ pertemuan php/latihan2.php
@@ -0,0 +1,20 @@
+<?php
+    $a = "hello";//string
+    $b = 20; // integer
+    $c = 3.15; //float
+    $d = true; // boolean
+
+    echo $a;
+    echo "<br>";
+    echo $b;
+    echo "<br>";
+    echo $c;
+    echo "<br>";
+    echo $d;
+    echo "<br>";
+
+    $nama ="helmi";
+    echo "nama ".$nama;
+    print "<br> <h1> PHP Keren!</h1>"
+
+?>
