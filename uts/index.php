3435e6bafa54864056860edb7fb01e7cbed13bce
helmiapriyanda <56975427+helmiapriyanda@users.noreply.github.com>
2020-11-24 13:38:00 +0700

    Update index.php

--- uts/index.php
+++ uts/index.php
@@ -16,7 +16,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-    <title>18104043_Ramadhan Rizqi Saputra</title>
+    <title>18104036_Helmi Apriyanda</title>
   </head>
   <body>
     <table class="table">
