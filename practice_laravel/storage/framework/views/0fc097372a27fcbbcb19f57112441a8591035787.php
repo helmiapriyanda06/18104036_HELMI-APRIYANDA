55838c351d1ddc8fa97578ab78d193c6c9a4f431
helmiapriyanda <56975427+helmiapriyanda@users.noreply.github.com>
2021-01-05 18:44:32 +0700

    laravel1,2,3

---
+++ practice_laravel/storage/framework/views/0fc097372a27fcbbcb19f57112441a8591035787.php
@@ -0,0 +1,8 @@
+
+<?php $__env->startSection('content'); ?>
+
+<h1>About</h1>
+<P>ini adalah halaman about</P>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('template.base', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\18104036_HELMI-APRIYANDA\practice_laravel\resources\views/about.blade.php ENDPATH**/ ?>
