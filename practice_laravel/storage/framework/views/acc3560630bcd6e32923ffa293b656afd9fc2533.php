91d97f7420a5b634ee8cfcd16519515059a5deb3
helmiapriyanda <56975427+helmiapriyanda@users.noreply.github.com>
2021-01-05 18:50:11 +0700

    praktikum laraver 1,2,3

--- practice_laravel/storage/framework/views/acc3560630bcd6e32923ffa293b656afd9fc2533.php
+++
@@ -1,9 +0,0 @@
-
-<?php $__env->startSection('content'); ?>
-
-<h1>Beranda</h1>
-<P>SELAMAT DATANG</P>
-
-
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('template.base', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\18104036_HELMI-APRIYANDA\practice_laravel\resources\views/beranda.blade.php ENDPATH**/ ?>
