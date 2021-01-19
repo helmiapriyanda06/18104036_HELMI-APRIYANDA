7eeb3ed985d055f7842a01f0b026a181db9d7b1e
helmiapriyanda <56975427+helmiapriyanda@users.noreply.github.com>
2021-01-19 18:18:11 +0700

    revisi

--- practice_laravel/lav/resources/views/template/base.blade.php
+++ practice_laravel/lav/resources/views/template/base.blade.php
@@ -41,7 +41,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22 /js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
     @yield('script')
   </body>
 </html>
